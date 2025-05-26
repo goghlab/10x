7fc9936746c3c78567828aebee5486c57ddf39bb
dom <dom851013@gmail.com>
2025-05-26 15:51:05 +0800

    Initial commit

---
+++ copilot-fully-custom/components/chat/Header.tsx
@@ -0,0 +1,12 @@
+import { Avatar, Format, AvatarSize} from "@leafygreen-ui/avatar";
+import Badge from "@leafygreen-ui/badge";
+
+export function Header() {
+    return (
+        <div className="flex items-center justify-center gap-2 border py-4 rounded-t-xl bg-white">
+            <img src="/V.png" alt="V Sir" className="w-10 h-10 rounded-full" />
+            <span className="text-lg font-bold">10X超絕游泳極速學習法</span>
+            <Badge variant="blue">Beta</Badge>
+        </div>
+    )
+}
